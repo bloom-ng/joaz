32896fd59cddba5621a50d3438a16cdb4bc41fbd
Lekantheboy <olusanuemmanuel@gmail.com>
2025-08-03 12:04:50 +0100

    sunday evening

--- routes/web.php
+++ routes/web.php
@@ -31,7 +31,7 @@ Route::get('/', function () {
     return view('welcome');
 })->name('home');
 
-Route::get('/login', function () {
+Route::get('/login-test', function () {
     return view('auth.login');
 })->name('login');
 
